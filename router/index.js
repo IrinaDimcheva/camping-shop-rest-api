316b79cafa40cbdc1650f22b9cb6c018e6d415cb
iamirinad <irka_dimcheva@abv.bg>
2023-02-23 22:39:00 +0200

    fixes

--- router/index.js
+++ router/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const user = require('./user');
 const products = require('./products');
-const likes = require('./likes');
 const auth = require('./auth');
 const orders = require('./orders');
 
@@ -9,7 +8,6 @@ router.use('/auth', auth);
 router.use('/user', user);
 router.use('/products', products);
 router.use('/orders', orders);
-router.use('/likes', likes);
 router.use('/favorites', favorites);
 
 module.exports = router;
