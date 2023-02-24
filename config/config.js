df776ed4ce67c94f63499a524b2e9809e290b3b4
iamirinad <irka_dimcheva@abv.bg>
2023-02-24 18:43:52 +0200

    change config

--- config/config.js
+++ config/config.js
@@ -7,10 +7,10 @@ const config = {
 		origin: ['http://localhost:5555', 'http://localhost:4200', 'http://localhost:3000', 'http://localhost:3001']
 	},
 	production: {
-		port: process.env.PORT || 5000,
-		// dbURL: process.env.DB_URL_CREDENTIALS,
-		dbURL: process.env.MONGO_ATLAS_DB_URL,
-		origin: []
+		// port: process.env.PORT || 5000,
+		// // dbURL: process.env.DB_URL_CREDENTIALS,
+		// dbURL: process.env.MONGO_ATLAS_DB_URL,
+		// origin: []
 	}
 };
 
